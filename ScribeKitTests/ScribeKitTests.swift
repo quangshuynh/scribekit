4fe08e65f6b7aff98fd56459b22afadbc84e5949
Quang <20378quang@gmail.com>
2026-08-28 22:19:36 -0400

    Initial Commit

---
+++ ScribeKitTests/ScribeKitTests.swift
@@ -0,0 +1,19 @@
+//
+//  ScribeKitTests.swift
+//  ScribeKitTests
+//
+//  Created by q on 8/28/26.
+//
+
+import Testing
+@testable import ScribeKit
+
+struct ScribeKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
