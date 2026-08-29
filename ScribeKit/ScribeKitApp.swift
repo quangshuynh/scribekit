7d4e5a5452cc212fcb0c65d633362461118f4a80
Quang <20378quang@gmail.com>
2026-08-28 22:19:36 -0400

    Initial Commit

---
+++ ScribeKit/ScribeKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScribeKitApp.swift
+//  ScribeKit
+//
+//  Created by q on 8/28/26.
+//
+
+import SwiftUI
+
+@main
+struct ScribeKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
